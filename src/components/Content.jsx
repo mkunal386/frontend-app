ba05a79a662e7fa6ba7bbd2f2b6130cf0481cc8f
Kunal <mkunal0803@gmail.com>
2026-03-11 14:52:38 +0530

    placed image at start in the box

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,45 +1,46 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./Content.css";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-const API_URL=import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL
 function Content() {
-//   const [count, setCount] = useState(0);
-  const [products, setProducts] = useState([]);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount(count - 1);
-  };
-  const fetchProducts = async () => {
-    const url = `${API_URL}/store`;
-    const res = await axios.get(url);
-    setProducts(res.data);
-  };
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-  return (
-    <div className="content">
-      {/* <h3>Products Page</h3> */}
-      {/* <button onClick={decrement}>-</button>
+    //   const [count, setCount] = useState(0);
+    const [products, setProducts] = useState([]);
+    const increment = () => {
+        setCount(count + 1);
+    };
+    const decrement = () => {
+        setCount(count - 1);
+    };
+    const fetchProducts = async () => {
+        const url = `${API_URL}/store`;
+        const res = await axios.get(url);
+        setProducts(res.data);
+    };
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+    return (
+        <div className="content">
+            {/* <h3>Products Page</h3> */}
+            {/* <button onClick={decrement}>-</button>
       {count}
       <button onClick={increment}>+</button>
       <hr /> */}
-      <div className="row">
-        {products.map((product) => (
-          <div className="box" >
-            <h3  >{product.name}</h3>
-            <p>{product.desc} </p>
-            <h4>{product.price}</h4>
-            <button><Link to="/cart">Add to Cart</Link></button>
-            <img src={`${API_URL}/${product.imageUrl}`}  width="300px"alt="" />
+            <div className="row">
+                {products.map((product) => (
+                    <div className="box" >
+                        <img src={`${API_URL}/${product.imageUrl}`} width="300px" alt="" />
+                        <h3  >{product.name}</h3>
+                        <p>{product.desc} </p>
+                        <h4>{product.price}</h4>
+                        <button><Link to="/cart">Add to Cart</Link></button>
+
+                    </div>
+                ))}
             </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 export default Content;
